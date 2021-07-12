ebf6dfb92c08610dff144586d4ee233fc4802bac
kumar thapa <kumarthapa@users.noreply.github.com>
2021-07-12 15:12:43 +0530

    Edit some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Testing from './components/Testing'
 function App() {
   return (
    <>
     <Navbar/>
     <Home/>
-    <Testing/>
    </>
   );
 }
